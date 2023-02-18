698c01af5ce88dbd0114a66fa7530562e847f0e7
wvxbs <gabriel.ferreira7854@gmail.com>
2023-02-18 17:16:20 -0300

    started working on the layout

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import Header from "../components/Header"
+
+const Home = () => {
+    return (
+        <div className="container-fluid">
+            <h1 className="title display-2 fw-bold">Gabriel Ferreira</h1>
+            <Header />
+        </div>
+    )
+}
+
+export default Home
